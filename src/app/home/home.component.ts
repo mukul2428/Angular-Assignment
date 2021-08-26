8cb47c639b11af7c07d5ebb205a5f2c756fff96c
Mukul Raghav <mukulraghav28@gmail.com>
2021-08-26 22:23:03 +0530

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  arr = ['First Line','Second Line', 'Third Line', 'Fourth Line'];
+
+}
